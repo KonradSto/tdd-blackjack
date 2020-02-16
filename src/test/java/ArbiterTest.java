6fb7855e5f545d691316e6bb8e78c4c74dfe5f4c
konradsto <stolarczyk.konrad@outlook.com>
2020-02-16 19:17:42 +0100

    initial commit

---
+++ src/test/java/ArbiterTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class ArbiterTest {
+
+    @Test
+    public void shouldReturnTrueIfPlayerWon() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
